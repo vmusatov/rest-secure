c858f927a076dafcb5c870d42ef7366dd9ef9275
vmusatov <v.musatov95@gmail.com>
2020-08-04 18:40:21 +0300

    create parent module

--- rest-secure/src/test/java/com/restsecure/BaseTest.java
+++ rest-secure/src/test/java/com/restsecure/BaseTest.java
@@ -12,6 +12,7 @@ import lombok.Data;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.testng.asserts.SoftAssert;
 
 import java.util.Arrays;
 import java.util.List;
